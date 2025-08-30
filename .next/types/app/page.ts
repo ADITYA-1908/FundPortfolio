a6641259df288cbb2e0c42d7d4a0fdb5f9a2ecd2
ADITYA BASANTIA <adityabasantia729@gmail.com>
2025-08-30 10:46:54 +0530

    APP

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: D:\Project\Full Stack\FundPortfolio\app\page.tsx
+
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App';
+// import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+
+// pages/_app.tsx
+import type { AppProps } from 'next/app'
+import './index.css'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
